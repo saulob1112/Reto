0ab29f4ecc13e4a5164b10d9b998fed9da766ac5
saulob <saul.ob1112@gmail.com>
2025-05-02 00:44:22 -0600

    Creacion de proyecto de productos

---
+++ src/test/java/com/reto/test/ProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
